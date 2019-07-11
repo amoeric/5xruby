70d9db6dece9f3ad26589ca5191dcc82fd486089
Eric Syu <xeriok02390@gmail.com>
2019-07-11 15:20:48 +0800

    add helper chrome

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,17 @@ require 'rspec/rails'
 require 'capybara/rails'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'chromeOptions' => {
+      'args' => ['--headless', '--disable-gpu']
+    }
+  )
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  end
+  Capybara.javascript_driver = :chrome
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
