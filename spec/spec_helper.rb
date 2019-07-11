309aee87452d59cf37c95aa184f965bb63830b60
Eric Syu <xeriok02390@gmail.com>
2019-07-11 18:32:14 +0800

    test3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   Capybara.register_driver :headless_chrome do |app|
 
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu) }
+      chromeOptions: { args: %w(no-sandbox headless disable-gpu) }
     )
     client = Selenium::WebDriver::Remote::Http::Default.new
     client.read_timeout = 120
