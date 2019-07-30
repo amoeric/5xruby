3804f29900f654f4c1f71eb11b1799ca53de9997
Eric Syu <xeriok02390@gmail.com>
2019-07-30 13:42:07 +0800

    all_spec update and finished step 16

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -2,25 +2,30 @@ require 'spec_helper'
 require 'date'
 
 feature "任務管理系統" do
+  let(:user_create) do
+    @user1 = User.create(account: 'zxc123', password: '123456')
+    @user2 = User.create(account: 'eric', password: '123456')
+  end
+  
+  before do
+    user_create
+    user_login(account:@user1.account)
+  end
+
+  after do
+    Mission.destroy_all
+  end
 
   context "任務的CRUD" do
     scenario "可新增自己的任務" do
-      create_user(account: 'zxc123', password: '123456')
-      user1 = User.last
-      expect(user1.account).to eq "zxc123"
-      create_user(account: 'eric', password: '123456')
-      user2 = User.last
-      expect(user2.account).to eq "eric"
-      expect(page).to have_content "eric"
-      user_login(account:'zxc123')
       expect(page).to have_content "新增任務"
-      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
-      #透過這方法比對資料庫資料
-      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
+      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
     end
   
     scenario "可查看自己的任務" do
-      user_login(account:'zxc123')
+      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
       expect(page).to have_content "任務二"
       page.first('div.mission', :text => '任務二').click_on I18n.t("mission.check")
       expect(page).to have_content "任務二"
@@ -28,16 +33,18 @@ feature "任務管理系統" do
     end
   
     scenario "可修改自己的任務" do
-      user_login(account:'zxc123')
+      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
       expect(page).to have_content "任務二"
-      edit_mission(mission:'任務二' ,title: '任務二', content: '五倍紅寶石', start_time: "2020-04-20 10:30", end_time: "2020-04-20 11:30", status: "待處理")
-      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-20 10:30", end_time: "2020-04-20 11:30", status: "待處理")
+      edit_mission(mission:'任務二' ,title: '任務二', content: '五倍紅寶石', start_time: "2020-04-20 10:30", end_time: "2020-04-20 11:30", status: "待處理", priority: "低", priority: "低")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-20 10:30", end_time: "2020-04-20 11:30", status: "待處理", priority: "低", priority: "低")
       expect(find('div.mission', :text => '任務二')).to have_content "2020-04-20 10:30:00 +0800"
       expect(find('div.mission', :text => '任務二')).to have_content "2020-04-20 11:30:00 +0800"
     end
   
     scenario "可刪除自己的任務" do
-      user_login(account:'zxc123')
+      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
       expect(page).to have_content "任務二"
       before_delete_mission_count = Mission.count
       delete_mission!(mission: "任務二")
@@ -48,91 +55,107 @@ feature "任務管理系統" do
   
   context "任務權限" do
     scenario "使用者登入後，只能看見自己建立的任務" do
-      user_login(account:'zxc123')
-      create_mission(title: '看不到此任務', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
-      check_mission(title: '看不到此任務', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
+      create_mission(title: '看不到此任務', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '看不到此任務', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
       expect(page).to have_content "看不到此任務"
       user_login(account:'eric')
-      create_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
-      check_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
+      create_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
       expect(page).to have_content "任務一"
       expect(page).to have_no_content "看不到此任務"
       page.should have_css("div.mission", :count => 1)
     end
   end
 
-  context "排序" do
-    let(:login){ user_login(account:'zxc123') }
-    let(:find_user) do
-      user = User.find_by_account('zxc123') 
-      user
-    end
-
-    before do
-      login
+  context "任務各種設定、排序" do
+    before do 
+      create_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-21 11:30:00", status: "待處理", priority: "中")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-21 11:30:00", status: "待處理", priority: "中")
     end
 
     scenario "可依照建立時間進行排序" do
-      create_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
-      check_mission(title: '任務一', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
       expect(page).to have_content "任務一"
-      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
-      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理")
       expect(page).to have_content "任務二"
-      #第一個mission 不是任務一  因為用了排序所以現在view是任務二在第一個
       click_on '以創建時間排序'
-      expect(page.first('div.mission')).to have_content "看不到此任務"
+      #第一個mission 不是任務一  因為用了排序所以現在view是任務二在第一個
+      page.should have_css("div.mission", :count => 2)
+      expect(page.first('div.mission')).to have_content "任務二"
       expect(page.first('div.mission')).to have_no_content "任務一"
-      #任務一、任務二、看不到此任務
-      page.should have_css("div.mission", :count => 3)
     end
     
     scenario "可設定任務的開始及結束時間" do
       expect(page).to have_content "查詢"
-      edit_mission(mission: '任務二', title: '任務二', content: '五倍紅寶石', start_time: "2020-04-21 10:30", end_time: "2020-04-21 11:30", status: "待處理")
-      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-21 10:30", end_time: "2020-04-21 11:30", status: "待處理")
-      expect(page.first('div.mission', :text => "任務二")).to have_content "2020-04-21 10:30:00 +0800"
-      expect(page.first('div.mission', :text => "任務二")).to have_content "2020-04-21 11:30:00 +0800"
+      edit_mission(mission: '任務二', title: '任務二', content: '五倍紅寶石', start_time: "2020-04-22 10:30", end_time: "2020-04-22 11:30", status: "待處理", priority: "中")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-22 10:30", end_time: "2020-04-22 11:30", status: "待處理", priority: "中")
+      expect(page.first('div.mission', :text => "任務二")).to have_content "2020-04-22 10:30:00 +0800"
+      expect(page.first('div.mission', :text => "任務二")).to have_content "2020-04-22 11:30:00 +0800"
     end
-    
+
+    scenario "可以依照開始時間排序" do
+    end
+
     scenario "可以依照結束時間排序" do
       click_on '以結束時間排序'
-      first_mission = find_user.missions.order(end_time: :asc).limit(10).first
-      last_mission = find_user.missions.order(end_time: :asc).limit(10).last
-      expect(first_mission.end_time).to eq Time.zone.parse("2020-04-19 11:30:00 +0800")
-      expect(last_mission.end_time).to eq Time.zone.parse("2020-04-21 11:30:00 +0800")
-      expect(find("div.end_time", match: :first)).to have_content "2020-04-19 11:30:00 +0800"
-      expect(all("div.end_time").last).to have_content "2020-04-21 11:30:00 +0800"
+      first_mission = find_user.missions.order(end_time: :desc).limit(10).first
+      last_mission = find_user.missions.order(end_time: :desc).limit(10).last
+      expect(find("div.end_time", match: :first)).to have_content first_mission.end_time
+      expect(all("div.end_time").last).to have_content last_mission.end_time
+    end
+    
+    scenario "可以依照優先順序排序" do
+      click_on '以優先權排序'
+      first_mission = find_user.missions.order(priority: :desc).limit(10).first
+      last_mission = find_user.missions.order(priority: :desc).limit(10).last
+      expect(find("div.priority", match: :first)).to have_content first_mission.priority
+      expect(all("div.priority").last).to have_content last_mission.priority
     end
   end
 
-  context "可設定任務目前的狀態（待處理、進行中、已完成）" do
-    let(:login){ user_login(account:'zxc123') }
-
-    before do
-      login
-    end
-
+  context "可設定任務狀態（待處理、進行中、已完成）、設定任務優先權（低、中、高）" do
     scenario "建立時能設定狀態" do
-      create_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中")
-      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中")
-      expect(find("div.mission", :text => "十八銅人")).to have_content enum_mission_status(value: "進行中")
+      create_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "低")
+      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "低")
+      expect(find("div.mission", :text => "十八銅人")).to have_content enum_mission(value: "進行中")
     end
 
     scenario "能修改任務狀態" do
-      edit_mission(mission: '十八銅人', title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成")
-      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成")
-      expect(find("div.mission", :text => "十八銅人")).to have_content enum_mission_status(value: "已完成")
+      create_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "低")
+      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "低")
+      edit_mission(mission: '十八銅人', title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成", priority: "低")
+      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成", priority: "低")
+      expect(find("div.mission", :text => "十八銅人")).to have_content enum_mission(value: "已完成")
+    end
+
+    scenario "建立時能設定優先權" do
+      create_mission(title: '任務優先權測試', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "中" )
+      check_mission(title: '任務優先權測試', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "中")
+      expect(find("div.mission", :text => "任務優先權測試")).to have_content enum_mission(value: "中")
+    end
+
+    scenario "能修改任務優先權" do
+      create_mission(title: '任務優先權測試', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "低" )
+      check_mission(title: '任務優先權測試', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "低" )
+      edit_mission(mission: '任務優先權測試', title: '任務優先權測試', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "高" )
+      check_mission(title: '任務優先權測試', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "進行中", priority: "高")
+      expect(find("div.mission", :text => "任務優先權測試")).to have_content enum_mission(value: "高")
     end
   end
-  
-  context "查詢" do
-    let(:login){ user_login(account:'zxc123') }
 
+  context "可以任務的標題、內容進行搜尋" do
     before do
-      login
+      #mission1
+      create_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成", priority: "低")
+      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成", priority: "低")
+      #mission2
+      create_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      check_mission(title: '任務二', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "低")
+      #mission3
+      create_mission(title: '任務三', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "高")
+      check_mission(title: '任務三', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "待處理", priority: "高")
     end
-
+    
     scenario "依標題查詢" do
       within '#title_search' do
         fill_in 'q[title_cont]', with: '十八銅人'
@@ -142,67 +165,48 @@ feature "任務管理系統" do
       expect(all("div.mission").last).to have_content "十八銅人"
     end
 
-    scenario "依狀態查詢" do
-      edit_mission(mission: '十八銅人', title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成")
-      check_mission(title: '十八銅人', content: '五倍紅寶石', start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30", status: "已完成")
-      within '#status_search' do
+    scenario "可依狀態篩選任務" do
+      within '#title_search' do
         select "已完成", :from => "q[status_eq]" 
       end
-      click_on "狀態查詢"
-      expect(page.first("div.mission")).to have_content enum_mission_status(value: "已完成")
-      expect(all("div.mission").last).to have_content enum_mission_status(value: "已完成")
-      reset!
+      click_on "查詢"
+      expect(page.first("div.mission")).to have_content enum_mission(value: "已完成")
+      expect(all("div.mission").last).to have_content enum_mission(value: "已完成")
+    end
+
+    scenario "可依優先權篩選任務" do
+      within '#title_search' do
+        select "高", :from => "q[priority_eq]" 
+      end
+      click_on "查詢"
+      expect(page.first("div.mission")).to have_content enum_mission(value: "高")
+      expect(all("div.mission").last).to have_content enum_mission(value: "高")
     end
   end
   
-  scenario "可設定任務的優先順序（高、中、低）" do
-  end
-
-  scenario "可依狀態篩選任務" do
-  end
-
-  scenario "可以任務的標題、內容進行搜尋" do
-  end
+  
 
   scenario "可為任務加上分類標籤" do
   end
-
-  scenario "任務列表，並可依優先順序、開始時間及結束時間等進行排序" do
-  end
   
   #user相關
-  def reset!
-    User.destroy_all
-    Mission.destroy_all
-  end
-  
-  def create_user(account: , password: )
-    visit '/users'
-    expect(page).to have_content I18n.t("user.new")
-    click_on I18n.t("user.new")
-    expect(page).to have_content I18n.t("user.new")
-    fill_in 'user[account]', with: account
-    fill_in 'user[password]', with: password
-    click_on '送出'
-    expect(page).to have_content account
-  end
-  
   def user_login(account: )
     visit '/users'
     page.find('div.user', :text => account).click_on '查看任務'
   end
   
   #mission相關
-  def check_mission(title: , content: , start_time: , end_time: , status: )
+  def check_mission(title: , content: , start_time: , end_time: , status: , priority: )
     mission = Mission.last
     expect(mission.title).to eq title
     expect(mission.content).to eq content
     expect(mission.start_time).to eq Time.zone.parse(start_time)
     expect(mission.end_time).to eq Time.zone.parse(end_time)
-    expect(mission.status).to eq enum_mission_status(value: status)
+    expect(mission.status).to eq enum_mission(value: status)
+    expect(mission.priority).to eq enum_mission(value: priority)
   end
 
-  def create_mission(title: , content: , start_time: , end_time: , status: )
+  def create_mission(title: , content: , start_time: , end_time: , status: , priority: )
     click_on I18n.t("back.new_mission")
     expect(page).to have_content I18n.t("back.mission_list")
     within '#new_mission' do
@@ -220,14 +224,15 @@ feature "任務管理系統" do
       select convert_date(end_time)[2], :from => "mission[end_time(3i)]" #日
       select convert_date(end_time)[3], :from => "mission[end_time(4i)]" #時
       select convert_date(end_time)[4], :from => "mission[end_time(5i)]" #分
-      find("span.radio", :text => status).click
+      find(".mission_status", text: "Status").find("span.radio", :text => status).click
+      find(".mission_priority", text: "Priority").find("span.radio", :text => priority).click
     end
     click_on '送出'
   end
 
-  def edit_mission(mission: , title: , content: , start_time: , end_time: , status: )
+  def edit_mission(mission: , title: , content: , start_time: , end_time: , status: , priority: )
     page.first('div.mission', :text => mission).click_on I18n.t("mission.edit")
-    expect(find_field('mission[title]').value) == mission
+    expect(find_field('mission[title]').value).to eq mission
     within 'form' do
       fill_in 'mission[title]', with: title
       fill_in 'mission[content]', with: content
@@ -243,7 +248,8 @@ feature "任務管理系統" do
       select convert_date(end_time)[2], :from => "mission[end_time(3i)]" #日
       select convert_date(end_time)[3], :from => "mission[end_time(4i)]" #時
       select convert_date(end_time)[4], :from => "mission[end_time(5i)]" #分
-      find("span.radio", :text => status).click
+      find(".mission_status", text: "Status").find("span.radio", :text => status).click
+      find(".mission_priority", text: "Priority").find("span.radio", :text => priority).click
     end
     click_on '送出'
   end
@@ -261,7 +267,7 @@ feature "任務管理系統" do
     changed_time = time.strftime('%Y:%b:%d:%H:%M').split(':') #["2011", "May", "19", "10", "30", "14"]
   end
 
-  def enum_mission_status(value: )
+  def enum_mission(value: )
     case value
     when "待處理"
       "waiting"
@@ -269,6 +275,12 @@ feature "任務管理系統" do
       "conduct"
     when "已完成"
       "finished"
+    when "低"
+      "low"
+    when "中"
+      "medium"
+    when "高"
+      "hight"
     end
   end
 end
--- spec/feature/mission_spec.rb
+++ spec/feature/mission_spec.rb
@@ -6,63 +6,64 @@ RSpec.describe Mission, type: :model do
     login_user = User.create( account: "amoeric", password: "123456" ) if login_user.nil?
     login_user
   end
-  
-  let(:reset!) do
-    User.destroy_all
-    Mission.destroy_all
-  end
 
   describe "mission model 測試" do 
     let(:mission) do
-      Mission.new( title: "18person", user_id: user.id )  
+      @mission_test = Mission.new( title: "18person", user_id: user.id )  
+    end
+
+    before do
+      mission
     end
 
     it "user_id必須存在,belons_to" do
-      mission = Mission.create( title: "18person", content:"5xruby")
-      expect(mission.errors[:user]).to eq ["必須存在"]
+      @mission_test.user_id = nil
+      @mission_test.valid?
+      expect(@mission_test.errors[:user]).to include "必須存在"
     end
 
     it "title不得為空" do
-      mission_notitle = Mission.create( content:"5xruby", user_id: user.id )
-      expect(mission_notitle.errors[:title]).to eq ["不能為空白"]
+      @mission_test.title = nil
+      @mission_test.valid?
+      expect(@mission_test.errors[:title]).to include "不能為空白"
     end
     
     it "content為空也給過" do
-      expect(mission).to be_valid
+      expect(@mission_test).to be_valid
     end
     
+    it "mission的結束時間不能比開始時間早" do
+      @mission_test.end_time = "2020-05-19 10:30:00"
+      @mission_test.start_time = "2020-05-19 11:30:00"
+      @mission_test.valid?
+      expect(@mission_test.errors[:end_time]).to include "結束日期不能比開始日期早哦！"
+    end
+
     context "enum測試" do
       it "priority是否有預設值" do
-        expect(mission.priority).to eq "low"
+        expect(@mission_test.priority).to eq "low"
       end
   
       it "priority的enum是否有效" do
-        mission.priority = 2
-        expect(mission.priority).to eq "hight"
+        @mission_test.priority = 2
+        expect(@mission_test.priority).to eq "hight"
       end
   
       it "status是否有預設值" do
-        expect(mission.status).to eq "waiting"
+        expect(@mission_test.status).to eq "waiting"
       end
   
       it "status的enum是否有效" do
-        mission.status = 2
-        expect(mission.status).to eq "finished"
+        @mission_test.status = 2
+        expect(@mission_test.status).to eq "finished"
       end
-    end
-
-    it "mission的結束時間不能比開始時間早" do
-      mission.end_time = "2020-05-19 10:30:00"
-      mission.start_time = "2020-05-19 11:30:00"
-      mission.save
-      expect(mission.errors[:end_time]).to eq ["結束日期不能比開始日期早哦！"]
     end
 
     context "搜尋" do
       let(:missions) do
-        Mission.create( title: "18person", content:"5xruby", user_id: user.id, status: 1, start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30" )
-        Mission.create( title: "18person", content:"ericisme", user_id: user.id, status: 1, start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30" )
-        Mission.create( title: "hellomission", content:"5xruby", user_id: user.id, status: 2, start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30" )
+        @mission_one = Mission.create( title: "18person", content:"5xruby", status: 1, priority: 0, user_id: user.id, start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30" )
+        @mission_two = Mission.create( title: "18person", content:"ericisme", status: 1, priority: 0, user_id: user.id, start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30" )
+        @mission_three = Mission.create( title: "hellomission", content:"5xruby", status: 2, priority: 2, user_id: user.id, start_time: "2020-04-19 10:30", end_time: "2020-04-19 11:30" )
       end
 
       before do
@@ -71,14 +72,23 @@ RSpec.describe Mission, type: :model do
        
       it "以標題搜尋" do
         @q = user.missions.ransack(title_cont: 'mission')
-        expect(@q.result.map(&:title)).to include ("hellomission")
+        expect(@q.result).to include (@mission_three)
       end
   
       it "以狀態搜尋" do
         @q = user.missions.ransack(status_eq: 2)
-        expect(@q.result.map(&:title)).to include ("hellomission")
+        expect(@q.result).to include (@mission_three)
+      end
+
+      it "以優先權搜尋" do
+        @q = user.missions.ransack(priority_eq: 2)
+        expect(@q.result).to include (@mission_three)
         reset!
       end
     end
+  end
+  def reset!
+    User.destroy_all
+    Mission.destroy_all
   end
 end
