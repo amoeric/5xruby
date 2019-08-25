e0927cb9f9b4b6afbd585d6c30bc4e9db5ef7e18
Eric Syu <xeriok02390@gmail.com>
2019-08-25 14:47:05 +0800

    current_user? rename to only_current_user

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -15,7 +15,7 @@ module SessionHelper
     end
   end
 
-  def current_user?
+  def only_current_user
     params_user = request.path.split("/")[2].to_i
     redirect_to user_missions_path(current_user), notice: I18n.t("message.not_self")  if params_user != current_user.id
   end
@@ -26,6 +26,6 @@ module SessionHelper
 
   def authenticate_user!
     redirect_to root_path, notice: I18n.t("notice.login_first") if current_user.blank? 
-    current_user?
+    only_current_user
   end
 end
